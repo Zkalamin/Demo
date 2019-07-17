3f4573284cf175339e06cbc877ea4c3724d6f74c
Alamin <zkalamin22@gmial.com>
2019-07-18 00:07:50 +0600

    third edition

---
+++ src/D.java
@@ -0,0 +1,32 @@
+public class D {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
